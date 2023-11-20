fbbbe1fcd658f018ded7378c48a0125134f70372
Youngseo <youngseo9603@gmail.com>
2023-11-20 09:34:00 +0900

    erase address repository : not table

--- trip/src/main/java/com/example/trip/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.trip.repository;
-
-import com.example.trip.domain.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
