a3f7d1a04b9d4da58c45bbe0d92e795dd844c2e5
pty9714 <pty9714@naver.com>
2023-11-23 17:55:14 +0900

    1123

--- frontend/src/views/user/MypageView.vue
+++ frontend/src/views/user/MypageView.vue
@@ -1,38 +1,60 @@
 <template>
-	<div class="grid grid-cols-4 gap-4" style="width: 100%; height: 1500px">
-		<div class="col-span-1" style="height: 100%">
-			<br />
-			<br />
-			<br />
-			<br />
-			<div class="text-center">{{ member.name }}</div>
-			<div class="text-center">{{ member.email }}</div>
-			<br />
-			<br />
+	<div class="flex" style="width: 100%; height: 1500px">
+		<div class="bg-gray-800 menu" style="height: 700px; width: 220px">
 			<ul>
-				<router-link to="/mypage/"
-					><li>
-						<img src="@/assets/icon/info.svg" alt="SVG Icon" />내 정보
+				<router-link
+					to="/mypage"
+					:class="{ selected: $route.path === '/mypage' }"
+				>
+					<li>
+						<img src="@/assets/icon/info.svg" alt="SVG Icon" />
+						<p :class="{ info: $route.path === '/mypage' }">내 정보</p>
 					</li></router-link
 				>
-				<router-link to="/mypage/mytrip"
-					><li>
-						<img src="@/assets/icon/earth.svg" alt="SVG Icon" />내 여행계획
-					</li></router-link
+				<hr />
+				<router-link
+					to="/mypage/mytrip"
+					:class="{ selected: $route.path === '/mypage/mytrip' }"
 				>
-				<router-link to="/mypage/mycomment"
-					><li>
-						<img src="@/assets/icon/comment.svg" alt="SVG Icon" />내 위시리스트
-					</li></router-link
+					<li>
+						<img src="@/assets/icon/earth.svg" alt="SVG Icon" />
+						<p :class="{ plan: $route.path === '/mypage/mytrip' }">
+							내 여행계획
+						</p>
+					</li>
+				</router-link>
+				<hr />
+
+				<router-link
+					to="/mypage/mycomment"
+					:class="{ selected: $route.path === '/mypage/mycomment' }"
 				>
-				<router-link to="/mypage/mypost"
-					><li>
-						<img src="@/assets/icon/pencil.svg" alt="SVG Icon" />내가 쓴 글
-					</li></router-link
+					<li>
+						<img src="@/assets/icon/comment.svg" alt="SVG Icon" />
+						<p :class="{ wishlist: $route.path === '/mypage/mycomment' }">
+							내 위시리스트
+						</p>
+					</li>
+				</router-link>
+				<hr />
+
+				<router-link
+					to="/mypage/mypost"
+					:class="{ selected: $route.path === '/mypage/mypost' }"
 				>
+					<li>
+						<img src="@/assets/icon/pencil.svg" alt="SVG Icon" />
+						<p :class="{ written: $route.path === '/mypage/mypost' }">
+							내가 쓴 글
+						</p>
+					</li>
+				</router-link>
+				<hr />
 			</ul>
 		</div>
-		<div class="col-span-3"><router-view></router-view></div>
+		<div class="mypageview">
+			<router-view></router-view>
+		</div>
 	</div>
 </template>
 
@@ -59,17 +81,23 @@ startMyPage();
 </script>
 
 <style lang="scss" scoped>
+.menu {
+	position: sticky;
+	top: 100px;
+}
 ul {
 	list-style-type: none;
+	font-size: 16px;
 	padding: 0;
 	margin: 0;
+	color: white;
 }
 
 li {
 	margin: 20px;
+	margin-left: 30px;
 	padding: 10px;
 	border-radius: 5px;
-	justify-content: center;
 	align-items: center;
 	display: flex;
 }
@@ -81,5 +109,17 @@ a {
 img {
 	width: 25px;
 	margin-right: 5px;
+}
+
+.mypageview {
+	padding: 50px;
+}
+.selected p {
+	font-weight: bold;
+}
+h2 {
+	font-size: 20px;
+}
+td {
 }
 </style>
